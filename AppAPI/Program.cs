14d3ceb8255dbe8d4d5704295d561fcdd1786112
BterceroIII <b1tercero.cruz@gmail.com>
2024-07-15 20:59:05 -0600

    build(appdbcontext.cs ): create data base and config models

--- AppAPI/Program.cs
+++ AppAPI/Program.cs
@@ -1,8 +1,16 @@
+using Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CadenaSQL"));
+});
 
 var app = builder.Build();
 
