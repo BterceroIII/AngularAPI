50b7f1c99126ddfc1e4d051b94804d7dc3c1a396
BterceroIII <b1tercero.cruz@gmail.com>
2024-07-21 20:56:35 -0600

    build(services): create interfaces empleado

---
+++ Services/Service/EmpleadoService.cs
@@ -0,0 +1,60 @@
+﻿using Data;
+using Microsoft.EntityFrameworkCore;
+using Models;
+using Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Services.Service
+{
+    public class EmpleadoService : IEmpleadoService
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public EmpleadoService(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+
+
+        public async Task<List<Empleado>> GetAllAsync()
+        {
+            return await _appDbContext.Empleados.ToListAsync();
+        }
+
+        public async Task AddAsync(Empleado empleado)
+        {
+            await _appDbContext.Empleados.AddAsync(empleado);
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        public async Task<Empleado> GetByIdAsync(int id)
+        {
+            return await _appDbContext.Empleados.FirstOrDefaultAsync(e => e.IdEmpleado == id);
+        }
+        
+        public async Task UpdateAsync(Empleado empleado)
+        {
+            _appDbContext.Empleados.Update(empleado);
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var empleado = await _appDbContext.Empleados.FirstOrDefaultAsync(e => e.IdEmpleado == id);
+
+            if (empleado != null)
+            {
+                _appDbContext.Empleados.Remove(empleado);
+                await _appDbContext.SaveChangesAsync();
+            }
+        }
+
+    }
+}
