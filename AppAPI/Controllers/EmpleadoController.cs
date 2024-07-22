78a3036f3c8318b48d90690c8fd3c505e78c5a1d
BterceroIII <b1tercero.cruz@gmail.com>
2024-07-21 21:36:24 -0600

    build(controllers): create empleado controller

---
+++ AppAPI/Controllers/EmpleadoController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using Services.Interface;
+
+namespace AppAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpleadoController : ControllerBase
+    {
+        private readonly IEmpleadoService _empleadoService;
+
+        public EmpleadoController(IEmpleadoService empleadoService)
+        {
+            _empleadoService = empleadoService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Lista()
+        {
+            List<Empleado> lista = await _empleadoService.GetAllAsync();
+            return StatusCode(StatusCodes.Status200OK, Lista);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Obtener(int id)
+        {
+            Empleado empleado = await _empleadoService.GetByIdAsync(id);
+            return StatusCode(StatusCodes.Status200OK, Lista);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Nuevo([FromBody] Empleado empleado)
+        {
+            await _empleadoService.AddAsync(empleado);
+            return StatusCode(StatusCodes.Status200OK, new {isSuccess = "Empleado creado exitosamente"});
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Editar([FromBody] Empleado empleado)
+        {
+            await _empleadoService.UpdateAsync(empleado);
+            return StatusCode(StatusCodes.Status200OK, new { isSuccess = "Empleado editado exitosamente" });
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Eliminar(int id)
+        {
+            await _empleadoService.DeleteAsync(id);
+            return StatusCode(StatusCodes.Status200OK, new { isSuccess = "Empleado Eliminado exitosamente" });
+        }
+    }
+}
